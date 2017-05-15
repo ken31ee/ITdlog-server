72ec63ada6cb69e5fe627b643edfa8d52dcd9e88
ken31ee <tungpakyin04@outlook.com>
2017-05-15 17:57:31 +0800

    ken31ee/ITdlog-issues#3 - Fix CROS problem when client connecting server

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var graphqlHTTP = require('express-graphql');
 import schema from './schema';
-var root = {
-    person: () => {
-        return 'Hello world!';
-    },
-};
+import cors from 'cors';
+
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', cors(), graphqlHTTP({
     schema,
     graphiql: true,
 }));
+
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
